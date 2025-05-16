e64a381709b935ef273729b7db92b4acd85d73b8
Murijioshavez <cecam.m.chavez7a@gmail.com>
2025-05-16 14:35:56 -0600

    clases Eric

--- Clase1.py
+++
@@ -1,9 +0,0 @@
-cant = 1
-cant2 = 10
-while cant <= 10 and cant2>=0:
-    print(cant, cant2)
-    cant+=1
-    cant2-=1
-
-
-
--- generador_correos.py
+++
@@ -1,4 +0,0 @@
-name = input('Ingrese su primer nombre ')
-apellido = input('Ingrese su primer apellido ')
-email = f'{name}.{apellido}@keyinsitute.edu.sv'
-print(email)
