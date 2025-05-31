b70d2e607ffd8e96caffa44159c016492649f566
Murijioshavez <cecam.m.chavez7a@gmail.com>
2025-05-31 09:22:03 -0600

    a

--- Clase6_ManejoDeListas/EliminandoDuplicados.py
+++ Clase6_ManejoDeListas/EliminandoDuplicados.py
@@ -2,9 +2,6 @@ entrada = input('Ingrese los numeros separados por espacios: ')
 lista = entrada.split()
 unicos = []
 for i in lista:
-    if i in unicos:
-        continue
-    else:
+    if i not in unicos:
         unicos.append(i)
-
 print(" ".join(unicos))
